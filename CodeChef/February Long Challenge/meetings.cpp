4cde5d1d9bd0f6659f4bf65b923e99bafe6f0312
Kartik <kartikvishnoi778@gmail.com>
2021-02-07 18:15:04 +0530

    removed exe file and added rule in gitignore

---
+++ CodeChef/February Long Challenge/meetings.cpp
@@ -0,0 +1,6 @@
+/*Chef has scheduled a meeting with his friends at a time P. He has N friends 
+(numbered 1 through N); for each valid i, the i-th friend is available from a time 
+Li to a time Ri (both inclusive). For each friend, can you help Chef find out 
+if this friend will be able to attend the meeting? More formally, 
+check if Li≤P≤Ri for each valid i */
+
