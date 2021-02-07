fb4162f67e6fe7e53ea05707de1992ee125a35b3
Kartik <kartikvishnoi778@gmail.com>
2021-02-07 17:28:38 +0530

    Solved question 2

---
+++ CodeChef/February Long Challenge/maximize_func.cpp
@@ -0,0 +1,45 @@
+//You are given a sequence A1,A2,…,AN. Find the maximum value of the expression 
+//|Ax−Ay|+|Ay−Az|+|Az−Ax| over all triples of pairwise distinct valid indices (x,y,z)
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    int N;
+    cin>>T;
+    if(T < 1 || T > 5)
+        return 0;
+    else {
+    while(T--)
+    {
+        cin>>N;
+        if(N < 3 || N > 100000)
+            continue;
+        short a[N];
+        int currentSum=0, largestSum=0;
+
+        for(int i=0;i<N;i++)
+            cin>>a[i];
+        
+        for(int i=0;i<N;i++)
+        {
+            if(i == (N-2))
+                break;
+            else
+            {
+                // using std::abs() as just using abs() gives ambiguity error 
+                currentSum = (std::abs(a[i] - a[i+1])) + (std::abs(a[i+1] - a[i+2])) + (std::abs(a[i+2] - a[i]));
+                if(currentSum > largestSum)
+                    largestSum = currentSum;
+            }
+        }
+        cout<<largestSum<<endl;
+    }
+    }
+
+    return 0;
+}
